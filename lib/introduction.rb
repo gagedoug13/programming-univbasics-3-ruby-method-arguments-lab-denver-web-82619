c381789bbcff93854bf756398bf5eb6cc550aa34
Gage Thornberry <gagedoug13@gmail.com>
2019-08-13 02:31:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,8 +7,10 @@ def introduction_with_language(name,language)
 end
 
 def introduction_with_language_optional(name,language)
-  puts "Hi, my name is #{name} and I am learning to program in ruby."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+
 
 
 
